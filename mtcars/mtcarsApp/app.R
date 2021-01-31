7f4d6ca8b457dc4d08227175455d96bda0931a15
Yihun Zeleke <yihunzeleke@outlook.com>
2021-01-30 23:31:52 -0500

    file updated

--- mtcars/mtcarsApp/app.R
+++ mtcars/mtcarsApp/app.R
@@ -1,15 +1,30 @@
-library(shinydashboard)
+library(ggplot2)
+library(dplyr)
 library(shiny)
+library(shinydashboard)
 
-library(shiny)
-library(shinydashboard)
 df <- mtcars
+
 ui <- dashboardPage(
     dashboardHeader(),
-    dashboardSidebar(),
-    dashboardBody()
+    dashboardSidebar(
+        selectInput("X", "Select Names", choices = c("mpg","disp","hp","drat","qsec")),
+        selectInput("Y", "Select Other ", choices = c("disp","drat","vs", "carb"))
+    ),
+    dashboardBody(
+        fluidRow(
+            plotOutput("distplot")
+        )
+    )
 )
 
-server <- function(input, output) { }
+server <- function(input, output) {
+    output$distplot <- renderPlot({
+            ggplot(df,aes_string(x = input$X, y = input$Y))+
+            geom_point()
+            
+    })
+}
 
 shinyApp(ui, server)
+
