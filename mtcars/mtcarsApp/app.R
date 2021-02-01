a2bfa3299ed9a48ec8d3fce488d7dcc48e2f38de
Yihun Zeleke <yihunzeleke@outlook.com>
2021-02-01 00:25:02 -0500

    file update

--- mtcars/mtcarsApp/app.R
+++ mtcars/mtcarsApp/app.R
@@ -1,28 +1,43 @@
 library(ggplot2)
 library(dplyr)
+library(tidyverse)
+library(magrittr)
 library(shiny)
 library(shinydashboard)
 
-df <- mtcars
+df_cars <- mtcars %>% 
+    rownames_to_column(var ="Car") %>% 
+    mutate(Car = strsplit(Car," ") %>% sapply(extract2, 1))
+    
+
 
 ui <- dashboardPage(
     dashboardHeader(),
     dashboardSidebar(
         selectInput("X", "Select Names", choices = c("mpg","disp","hp","drat","qsec")),
-        selectInput("Y", "Select Other ", choices = c("disp","drat","vs", "carb"))
+        selectInput("Y", "Select Other ", choices = c("disp","drat","vs", "carb")),
+        selectInput("car_table", "Select car Brand", choices = arrange(distinct(df_cars, Car)), selected = "Merc")
     ),
     dashboardBody(
         fluidRow(
-            plotOutput("distplot")
+            plotOutput("distplot"),
+            tableOutput("tbl")
         )
     )
 )
 
 server <- function(input, output) {
     output$distplot <- renderPlot({
-            ggplot(df,aes_string(x = input$X, y = input$Y))+
+        df_cars %>% 
+            filter(df_cars == input$car_table) %>% 
+            ggplot(aes_string(x = input$X, y = input$Y))+
             geom_point()
             
+    })
+    
+    output$tbl <- renderTable({
+        df_cars %>% 
+            filter(Car == input$car_table) %>% arrange(Car)
     })
 }
 
