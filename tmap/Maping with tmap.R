59cec75bf73f862105d97d6ee9b80329758db0f3
Yihun Zeleke <43428631+yihunzeleke@users.noreply.github.com>
2021-09-08 22:06:50 -0500

    Update Maping with tmap.R

--- tmap/Maping with tmap.R
+++ tmap/Maping with tmap.R
@@ -24,6 +24,45 @@ tm_basemap(server = "OpenStreetMap.HOT")+
   tm_text("ZCTA5CE10", size = 0.5)+
   tm_layout(legend.outside = F,  legend.just = "center")
 
+#====================== Leaflet================================#
+library(leaflet)
+library(RColorBrewer)
+library(htmltools)
+
+pal <- colorNumeric("YlOrRd", domain = shp$CrudeRate)
+labels <- 
+  sprintf(
+    "<strong>%s</strong><br/> Case Rate: %s per 100k",
+    shp$ZCTA5CE10,shp$CrudeRate) %>% 
+  lapply(htmltools::HTML)
+
+
+leaflet(shp) %>%
+ # setView(-96, 37.8, 4) %>%
+  addProviderTiles("CartoDB.PositronNoLabels") %>%
+  addPolygons(
+    fillColor = ~pal(CrudeRate), 
+    weight = 1,
+    opacity = 1,
+    color = "white",
+    fillOpacity = 0.7,
+    highlight = highlightOptions(
+      weight = 2,
+      color = "#666",
+      fillOpacity = 0.7,
+      bringToFront = TRUE),
+    label = labels,
+    labelOptions = labelOptions(
+      style = list("font-weight" = "normal"),
+      textsize = "15px",
+      direction = "auto")) %>% 
+  addLegend(pal = pal,
+            values = shp$CrudeRate,
+            position = "bottomright",
+            title = "COVID-19 Case Rate",
+            labFormat = labelFormat(suffix = ""),
+            opacity = 0.8,
+            na.label = "NA")
 
 
 
